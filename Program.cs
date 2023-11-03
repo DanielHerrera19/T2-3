d24abc51beab41d154d4119d696c8d9763d7af7b
Daniel <Daniel@DESKTOP-KKC0TQU>
2023-11-03 16:46:10 -0500

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using T2_3.EJERCICIO_3;
+
+namespace T2_3
+{
+    internal class Program
+    { 
+        static void Main(string[] args)
+        {
+            int opcion = MenuConsola.MenuPrincipal();
+            do
+            {
+                switch (opcion)
+                {
+                    case 0:
+                        opcion = MenuConsola.MenuPrincipal();
+                        break;
+                    case 1:
+                        opcion = MenuConsola.sumar();
+                        break;
+                    case 2:
+                        opcion = MenuConsola.restar();
+                        break;
+                    case 3:
+                        opcion = MenuConsola.multiplicar();
+                        break;
+                    case 4:
+                        opcion = MenuConsola.dividir();
+                        break;
+                }
+            } while (opcion != 5);
+        }
+    }
+}
